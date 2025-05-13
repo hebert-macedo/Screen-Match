22cfa7c422041214e95c26a187865ae7e0beeca3
Hebert Rodrigues <hebert.macedo2@gmail.com>
2025-05-13 11:33:03 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+
+        String title = "Top Gun: Maverick";
+        int releaseYear = 2022;
+        boolean includedInPlan = true;
+
+        System.out.println("Filme: " + title);
+        System.out.println("Ano de lançamento: " + releaseYear);
+        System.out.println("Incluído no plano? " + includedInPlan);
+
+    }
+}
